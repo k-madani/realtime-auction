26fe6ea087f8d074969c8ea2645f83c8ece247d8
k-madani <krishnamadani@gmail.com>
2026-01-07 20:49:31 -0500

    created DTO

---
+++ realtime-auction/src/main/java/com/auction/realtime_auction/dto/AuthResponse.java
@@ -0,0 +1,13 @@
+package com.auction.realtime_auction.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class AuthResponse {
+    private String token;
+    private String username;
+    private String email;
+    private String role;
+}

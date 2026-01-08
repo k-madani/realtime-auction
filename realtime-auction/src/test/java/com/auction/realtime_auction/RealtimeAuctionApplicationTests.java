6ae3defcc24b9f007b74280c55815d5d4bd00fb1
k-madani <krishnamadani@gmail.com>
2026-01-07 20:40:31 -0500

    initial project structure & user model added

---
+++ realtime-auction/src/test/java/com/auction/realtime_auction/RealtimeAuctionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.auction.realtime_auction;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RealtimeAuctionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
